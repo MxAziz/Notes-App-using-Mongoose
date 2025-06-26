4d9b79953978058cdf7999580d5e2990a40c4443
MxAziz <hereabdulaziz@gmail.com>
2025-06-26 22:23:18 +0600

    added user model schema

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const user = mongoose.model('user', userSchema);
